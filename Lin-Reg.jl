6fd08fca4521f4362d4a665c821d91f475cf67ff
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2025-07-28 21:14:24 +0530

    Create Lin-Reg.jl

---
+++ Lin-Reg.jl
@@ -0,0 +1,27 @@
+using MLJ, MLJLinearModels, RDatasets, DataFrames, Plots, StatsBase, PrettyPrinting
+import DataFrames:describe, select, Not, rename!
+import RDatasets: dataset
+
+LinearRegressor = @load LinearRegressor pkg=MLJLinearModels
+boston = dataset("MASS", "Boston")
+first(boston, 10)
+
+describe(boston, :mean, :std, :eltype)
+data = coerce(boston, autotype(boston, :discrete_to_continuous))
+
+X = select(data, Not(:MedV))
+y = data.MedV
+
+linreg = LinearRegressor()
+
+X_uni = select(X, :LStat)
+mach_uni = MLJ.machine(linreg, X_uni, y)
+MLJ.fit!(mach_uni)
+
+fp = fitted_params(mach_uni)
+@show fp.coefs
+@show fp.intercept
+
+plot(X.LStat, y ,seriestype=:scatter, markershape=:circle, legend=true, size=(400,500))
+Xnew = (LStat = collect(range(extrema(X.LStat)...,length=150)),)
+plot!(Xnew.LStat, MLJ.predict(mach_uni, Xnew), linewidth=5, color=:red)
