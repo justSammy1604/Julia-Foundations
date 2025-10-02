27e57dad095602ecb962d3bb9f442bd9f6ef0cab
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2025-10-02 21:20:18 +0530

    Update Simple-ML-iris.jl

--- Simple-ML-iris.jl
+++ Simple-ML-iris.jl
@@ -1,4 +1,4 @@
-# Laying the basics of ML. Just a demo to show how ML works in Julia. Further will dive deep to enhance model performance.
+# Laying the basics of ML. Just a demo to show how ML works in Julia. Further will dive deep to enhance model performance. 
 using PrettyPrinting, StableRNGs, DecisionTree, MLJDecisionTreeClassifier, Statistics, StatsBase, RDatasets, Plots, MLJ
 
 iris_X, iris_y = @load_iris
