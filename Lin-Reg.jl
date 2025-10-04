684755659e33bd0551840704b9a8651079681fd9
Samuel Mesquita <100987969+justSammy1604@users.noreply.github.com>
2025-10-04 21:03:10 +0530

    Update Lin-Reg.jl

--- Lin-Reg.jl
+++ Lin-Reg.jl
@@ -3,7 +3,7 @@ import DataFrames:describe, select, Not, rename!
 import RDatasets: dataset
 
 LinearRegressor = @load LinearRegressor pkg=MLJLinearModels
-boston = dataset("MASS", "Boston")
+boston = dataset("MASS", "Boston") 
 first(boston, 10)
 last(boston, 20) 
 
